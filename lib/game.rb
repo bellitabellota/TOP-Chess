191d7a474593805f60b284fcb0007e9a5c237539
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-08 09:08:55 +0100

    Create #create_set method

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,32 @@
 require_relative "player"
+require_relative "sets"
 
 class Game
+  attr_reader :player1, :player2
+  attr_accessor :player1_set, :player2_set, :board
+
   def initialize
     @player1 = Player.new
+    @player1_set = nil
     @player2 = Player.new
+    @player1_set = nil
   end
 
   def play_game
     create_players
+
+    self.player1_set = create_set(player1)
+    self.player2_set = create_set(player2)
+
     display_board
+  end
+
+  def create_set(player)
+    if player.set_color == "white"
+      Set.new(0)
+    else
+      Set.new(1)
+    end
   end
 
   def create_players
