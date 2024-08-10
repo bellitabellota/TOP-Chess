11e66be16aec10e787144ff2c94c8f43311ce4bc
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-10 09:38:52 +0100

    Create King class

---
+++ lib/king/king.rb
@@ -0,0 +1,23 @@
+require_relative "king_graph"
+
+class King
+  attr_reader :token, :coordinates
+  attr_accessor :current_position
+
+  @@graph = KingGraph.new ###
+  @@start_coordinates = [[4, 0], [4, 7]]
+  @@king_tokens = ["\u265A", "\u2654"]
+
+  def initialize(token_index)
+    @token = @@king_tokens[token_index]
+    @coordinates = @@start_coordinates.pop
+  end
+
+  def self.graph
+    @@graph
+  end
+
+  def self.coordinates
+    @@start_coordinates
+  end
+end
