7625b4a5188ec80893946d54b15687ff7f372c49
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-10 11:02:42 +0100

    Create Bishop class

---
+++ lib/bishop/bishop.rb
@@ -0,0 +1,22 @@
+require_relative "bishop_graph"
+
+class Bishop
+  attr_reader :token, :coordinates
+
+  @@graph = BishopGraph.new
+  @@start_coordinates = [[2, 0], [5, 0], [2, 7], [5, 7]]
+  @@bishop_tokens = ["\u265D", "\u2657"]
+
+  def initialize(token_index)
+    @token = @@bishop_tokens[token_index]
+    @coordinates = @@start_coordinates.pop
+  end
+
+  def self.graph
+    @@graph
+  end
+
+  def self.coordinates
+    @@start_coordinates
+  end
+end
