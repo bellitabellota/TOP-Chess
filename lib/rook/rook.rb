b928ba6611fbbcd61eb1c41f18e188e03ada99db
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-10 10:18:18 +0100

    Create Rook class

---
+++ lib/rook/rook.rb
@@ -0,0 +1,22 @@
+require_relative "rook_graph"
+
+class Rook
+  attr_reader :token, :coordinates
+
+  @@graph = RookGraph.new
+  @@start_coordinates = [[0, 0], [7, 0], [0, 7], [7, 7]]
+  @@rook_tokens = ["\u265C", "\u2656"]
+
+  def initialize(token_index)
+    @token = @@rook_tokens[token_index]
+    @coordinates = @@start_coordinates.pop
+  end
+
+  def self.graph
+    @@graph
+  end
+
+  def self.coordinates
+    @@start_coordinates
+  end
+end
