e5041f91ed337b22b42991a0b21753ca78732d6a
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-10 11:25:16 +0100

    Create QueenGraph.rb

---
+++ lib/queen/queen_graph.rb
@@ -0,0 +1,22 @@
+require_relative "queen_vertex"
+
+class QueenGraph
+  attr_accessor :vertices
+
+  def initialize
+    @vertices = create_vertices
+  end
+
+  def create_vertices
+    array = []
+    value_on_x_axis = (0..7)
+    value_on_y_axis = (0..7)
+
+    value_on_x_axis.each do |x_value|
+      value_on_y_axis.each do |y_value|
+        array << QueenVertex.new(x_value, y_value)
+      end
+    end
+    array
+  end
+end
