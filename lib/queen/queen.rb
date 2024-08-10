91b54e0c648852ab46b33f6ccb026cbaa1e2636b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-10 11:24:49 +0100

    Create Queen class

---
+++ lib/queen/queen.rb
@@ -0,0 +1,22 @@
+require_relative "queen_graph"
+
+class Queen
+  attr_reader :token, :coordinates
+
+  @@graph = QueenGraph.new
+  @@start_coordinates = [[3, 0], [3, 7]]
+  @@queen_tokens = ["\u265B", "\u2655"]
+
+  def initialize(token_index)
+    @token = @@queen_tokens[token_index]
+    @coordinates = @@start_coordinates.pop
+  end
+
+  def self.graph
+    @@graph
+  end
+
+  def self.coordinates
+    @@start_coordinates
+  end
+end
