054dfd222f94b8f569e935ff32c2dbbc6f24bf4f
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-08-14 14:26:22 +0100

    Create tests for #verify_target_field_reachable?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,123 @@
-require_relative "lib/game"
+require_relative "../lib/game"
+
+describe Game do
+  subject(:game) { described_class.new }
+
+  describe "#verify_target_field_reachable?" do
+    context "when piece_to_move is a Knight" do
+      let(:knight) { Knight.new(0) }
+      let(:knight_indexes) { [6, 7] }
+
+      context "when target_field is reachable" do
+        let(:knight_target_field) { "f6" }
+        it "returns true" do
+          return_value = game.verify_target_field_reachable?(knight, knight_indexes, knight_target_field)
+          expect(return_value).to eq true
+        end
+      end
+
+      context "when target_field NOT reachable" do
+        let(:knight_target_field) { "f5" }
+        it "returns false" do
+          return_value = game.verify_target_field_reachable?(knight, knight_indexes, knight_target_field)
+          expect(return_value).to eq false
+        end
+      end
+    end
+
+    context "when piece_to_move is a Bishop" do
+      let(:bishop) { Bishop.new(0) }
+      let(:bishop_indexes) { [5, 7] }
+
+      context "when target_field is reachable" do
+        let(:bishop_target_field) { "d6" }
+        it "returns true" do
+          return_value = game.verify_target_field_reachable?(bishop, bishop_indexes, bishop_target_field)
+          expect(return_value).to eq true
+        end
+      end
+
+      context "when target_field NOT reachable" do
+        let(:bishop_target_field) { "e5" }
+        it "returns false" do
+          return_value = game.verify_target_field_reachable?(bishop, bishop_indexes, bishop_target_field)
+          expect(return_value).to eq false
+        end
+      end
+    end
+
+    context "when piece_to_move is a King" do
+      let(:king) { King.new(0) }
+      let(:king_indexes) { [4, 7] }
+      context "when target_field is reachable" do
+        let(:king_target_field) { "e7" }
+        it "returns true" do
+          return_value = game.verify_target_field_reachable?(king, king_indexes, king_target_field)
+          expect(return_value).to eq true
+        end
+      end
+
+      context "when target_field NOT reachable" do
+        let(:king_target_field) { "e8" }
+        it "returns false" do
+          return_value = game.verify_target_field_reachable?(king, king_indexes, king_target_field)
+          expect(return_value).to eq false
+        end
+      end
+    end
+
+    context "when piece_to_move is a Queen" do
+      let(:queen) { Queen.new(0) }
+      let(:queen_indexes) { [3, 7] }
+      context "when target_field is reachable" do
+        let(:queen_target_field) { "h4" }
+        it "returns true" do
+          return_value = game.verify_target_field_reachable?(queen, queen_indexes, queen_target_field)
+          expect(return_value).to eq true
+        end
+      end
+
+      context "when target_field NOT reachable" do
+        let(:queen_target_field) { "e3" }
+        it "returns false" do
+          return_value = game.verify_target_field_reachable?(queen, queen_indexes, queen_target_field)
+          expect(return_value).to eq false
+        end
+      end
+    end
+
+    context "when piece_to_move is a Pawn" do
+      let(:pawn) { Pawn.new(0) }
+      let(:pawn_indexes) { [7, 6] }
+      context "when target_field is a reachable vertix in the PawnGraph" do
+        let(:pawn_target_field) { "h5" }
+        it "returns true" do
+          allow(game).to receive(:reachable_in_graph_of_current_player?).and_return(true)
+          allow(game).to receive(:diagonal_move_possible?).and_return(false)
+          return_value = game.verify_target_field_reachable?(pawn, pawn_indexes, pawn_target_field)
+          expect(return_value).to eq true
+        end
+      end
+
+      context "when target_field is a diagonal field with piece of opponent" do
+        let(:pawn_target_field) { "g6" }
+        it "returns true" do
+          allow(game).to receive(:reachable_in_graph_of_current_player?).and_return(false)
+          allow(game).to receive(:diagonal_move_possible?).and_return(true)
+          return_value = game.verify_target_field_reachable?(pawn, pawn_indexes, pawn_target_field)
+          expect(return_value).to eq true
+        end
+      end
+
+      context "when target_field NOT reachable" do
+        let(:pawn_target_field) { "h5" }
+        it "returns false" do
+          allow(game).to receive(:reachable_in_graph_of_current_player?).and_return(false)
+          allow(game).to receive(:diagonal_move_possible?).and_return(false)
+          return_value = game.verify_target_field_reachable?(pawn, pawn_indexes, pawn_target_field)
+          expect(return_value).to eq false
+        end
+      end
+    end
+  end
+end
